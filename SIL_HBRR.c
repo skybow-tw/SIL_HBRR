4cab6b3deab7a5b980558f84a1471b0f1b6616f7
C.H. Lee <skybowii@gmail.com>
2022-07-22 17:32:51 +0800

    Remove pigpio.h ISR description and time.h usage

--- SIL_HBRR.c
+++ SIL_HBRR.c
@@ -1,55 +1,3 @@
-
-//========pigpio example========
-/*
-ALL GPIO are identified by their Broadcom number.
-
-*Usage*
-Include <pigpio.h> in your source files.
-
-Assuming your source is in prog.c use the following command to build and
-run the executable.
-
-. .
-gcc -Wall -pthread -o prog prog.c -lpigpio -lrt
-sudo ./prog
-. .
-
-For examples of usage see the C programs within the pigpio archive file.
-
-
-
-*Notes*
-
-All the functions which return an int return < 0 on error.
-
-[*gpioInitialise*] must be called before all other library functions
-with the following exceptions:
-
-. .
-[*gpioCfg**]
-[*gpioVersion*]
-[*gpioHardwareRevision*]
-. .
-
-If the library is not initialised all but the [*gpioCfg**],
-[*gpioVersion*], and [*gpioHardwareRevision*] functions will
-return error PI_NOT_INITIALISED.
-
-If the library is initialised the [*gpioCfg**] functions will return
-error PI_INITIALISED.
-
-If you intend to rely on signals sent to your application, you should
-turn off the internal signal handling as shown in this example:
-
-. .
-int cfg = gpioCfgGetInternals();
-cfg |= PI_CFG_NOSIGHANDLER;  // (1<<10)
-gpioCfgSetInternals(cfg);
-int status = gpioInitialise();
-. .
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -136,27 +84,7 @@ void myInterrupt0(int gpio, int level, uint32_t tick)
 int main()
 {
 
-  /*
-  Using "time.h"  to get current time and apply to datalog filename!
-  3 functions and 2 data type are used here.
-
-  NOTE: time_t"calendar time"-> struct tm "structure time data " -> formatted time string
-
-  Function "time_t time(time_t *timer)":
-  return current calendar time.
-
-  Function "struct tm *localtime(const time_t *timer)":
-  convert the "calendar time" to "tm" type which is expressed in the local time zone.
-
-  Function "size_t strftime(char *str, size_t maxsize, const char *format, const struct tm *timeptr)":
-  formats the time represented in the structure "timeptr"
-  according to the formatting rules defined in "format" and stored into "str".
-
-  NOTE: "time_t" is a type suitable for storing the calendar time.
-  NOTE: "tm" is a structure used to hold the time and date.
-
-
-  */
+  
 
   //  Declare a "time_t" type variable
   time_t t1 = time(NULL);
